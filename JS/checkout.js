1b60f12c595382f74fabc0e209b92c09c6466376
Noiana Lagras <noi-lagras@hotmail.com>
2023-03-04 03:32:01 -0300

    primer commit

---
+++ JS/checkout.js
@@ -0,0 +1,13 @@
+//Formularios de checkouts
+const formulario = document.getElementById("form");
+//datos de compras | financiocion
+const nombre = document.getElementById("firstName");
+const apellido = document.getElementById("lastName");
+const email = document.getElementById("email");
+const monto = document.getElementById("amount");
+const cuotas = document.getElementById("fees");
+const montoFinal = document.getElementById("finalAmount");
+const cuotasFinales = document.getElementById("finalFees");
+const intereses = document.getElementById("interests");
+const totalADevolver = document.getElementById("totalAmount");
+const guardarInfo = document.getElementById("simulate")
